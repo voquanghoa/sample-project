30595e8a844bd25d3f662f95bbd3ec4077bd2229
Vo Quang Hoa <voquanghoa@gmail.com>
2020-07-11 23:13:12 +0700

    Update

--- Bkdn.Website/Startup.cs
+++ Bkdn.Website/Startup.cs
@@ -35,7 +35,6 @@ namespace Bkdn.Website
             else
             {
                 app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
@@ -57,9 +56,6 @@ namespace Bkdn.Website
 
             app.UseSpa(spa =>
             {
-                // To learn more about options for serving an Angular SPA from ASP.NET Core,
-                // see https://go.microsoft.com/fwlink/?linkid=864501
-
                 spa.Options.SourcePath = "ClientApp";
 
                 if (env.IsDevelopment())
